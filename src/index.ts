97f648a75ff89ae1c27e828c88749f108614a560
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2024-06-17 18:47:39 +0300

    feat: 🚀 export imageLoader

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import imageLoader from './components/utils/imageLoader'
 import withExportImages from './withExportImages'
 
 export type { Config } from './utils/getConfig'
+
+export { imageLoader }
 
 export default withExportImages
 module.exports = withExportImages
